d1b4bc9240072742b05ed86c883a8340294ca8b8
Derek J Leong <derekjleong@gmail.com>
2019-04-30 16:09:52 -0700

    Updates

---
+++ precache-manifest.42f10ca56000975bca0365cb6f5b7c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e2810d4c9ba28065fb297e67908a58",
+    "url": "/react-router-app-template/index.html"
+  },
+  {
+    "revision": "a0c563876c36d207d9c9",
+    "url": "/react-router-app-template/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "39757c798cf2a4ece1b3",
+    "url": "/react-router-app-template/static/js/2.134937c6.chunk.js"
+  },
+  {
+    "revision": "a0c563876c36d207d9c9",
+    "url": "/react-router-app-template/static/js/main.98750856.chunk.js"
+  },
+  {
+    "revision": "816cac9834b7799c5147",
+    "url": "/react-router-app-template/static/js/runtime~main.e92d32c0.js"
+  }
+]);
